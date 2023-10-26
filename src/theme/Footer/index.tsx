e20e57d41197fdac460e658a2347d38b1c4cdee9
Alexander Blum <workispleasure@pleasureiswork.net>
2023-10-26 14:15:20 +0200

    hides footer in backend

--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -2,12 +2,17 @@
  * For copyright and license terms, see COPYRIGHT.md (top level of repository)
  * Repository: https://github.com/georga-app/georga-client-react
  */
+import { useContext } from 'react';
+
 import Box from '@mui/material/Box';
 
+import UserContext from '@/provider/User';
 import Menu from './Menu';
 import Funding from './Funding';
 
 function Footer() {
+  const user = useContext(UserContext);
+  if (user.isLoggedIn) return;
   return (
     <Box sx={{ paddingT: 2 }}>
       <Funding />
