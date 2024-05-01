096284a4131a9104b21955d29f03782c3ee91cdf
Alexander Blum <workispleasure@pleasureiswork.net>
2024-05-02 00:01:02 +0200

    fixes auth flow

--- src/components/person/PersonAuthFlow.tsx
+++ src/components/person/PersonAuthFlow.tsx
@@ -2,7 +2,9 @@
  * For copyright and license terms, see COPYRIGHT.md (top level of repository)
  * Repository: https://github.com/georga-app/georga-client-react
  */
-import { useContext, Suspense } from "react";
+'use client';
+
+import { useContext, useEffect, Suspense } from "react";
 import { useRouter } from 'next/navigation'
 import { useSearchParams } from 'next/navigation'
 
@@ -11,6 +13,16 @@ import UserContext from "@/provider/User";
 // import AdminLevel from '@/types/AdminLevel';
 import AdminLevel from '@/types/AdminLevel';
 import { TokenAuthMutation } from '@/types/__generated__/graphql';
+
+// see https://developer.school/snippets/react/localstorage-is-not-defined-nextjs
+let localStorage: Storage = (typeof window !== "undefined") ? window.localStorage : {
+  length: 0,
+  key: () => null,
+  clear: () => undefined,
+  getItem: () => null,
+  setItem: () => null,
+  removeItem: () => null,
+};
 
 function PersonAuthFlow() {
   // provider
@@ -22,7 +34,7 @@ function PersonAuthFlow() {
   const id = searchParams.get('id') || '';
   const token = searchParams.get('token') || '';
   const adminLevel = searchParams.get('adminLevel') || '';
-  const redirect = searchParams.get('redirect') || '/';
+  let redirect = searchParams.get('redirect') || '/';
   const embed = !!parseInt(searchParams.get('embed') || "");
   const emulator = !!parseInt(searchParams.get('emulator') || "");
 
@@ -31,24 +43,24 @@ function PersonAuthFlow() {
       || !['ORGANIZATION', 'PROJECT', 'OPERATION', 'NONE']
          .includes(adminLevel)) {
     user.logout();
-    router.push('/login')
-    return <></>;
+    redirect = '/login';
+  } else {
+    // embed
+    localStorage.setItem("embed", embed ? "1" : "0");
+    localStorage.setItem("emulator", emulator ? "1" : "0");
+
+    // auth
+    user.login({
+      id: id,
+      token: token,
+      adminLevel: adminLevel,
+    } as TokenAuthMutation['tokenAuth']);
   }
 
-  // embed
-  localStorage.setItem("embed", embed ? "1" : "0");
-  localStorage.setItem("emulator", emulator ? "1" : "0");
-
-  // auth
-  user.login({
-    id: id,
-    token: token,
-    adminLevel: adminLevel,
-  } as TokenAuthMutation['tokenAuth']);
-
   // redirect
-  router.push(redirect)
-
+  useEffect(() => {
+    router.push(redirect)
+  });
   // return
   return <></>;
 }
